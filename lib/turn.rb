c17e23ba015980ae4a5fdca7213281d6dbbfaeb6
Lee Michaeli <leeisr@gmail.com>
2020-09-03 01:58:11 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,14 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+#coverts a user's place on the board to the index integer
+def input_to_index(user_input)
+  index = user_input.to_i
+  index -= 1
+  return index
+end
